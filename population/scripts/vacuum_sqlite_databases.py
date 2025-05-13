5bf88136474aea2178835671e06dae8c903b7f97
philmorefield <philmorefield@users.noreply.github.com>
2025-05-12 21:24:00 -0400

    Update vacuum_sqlite_databases.py

--- population/scripts/vacuum_sqlite_databases.py
+++ population/scripts/vacuum_sqlite_databases.py
@@ -4,9 +4,12 @@ import sqlite3
 
 
 def main():
-    p = 'D:\\OneDrive\\ICLUS_v3\\population'
+    p = 'D:\\OneDrive'
 
     for db in glob.glob(os.path.join(p, '**\\*.sqlite'), recursive=True):
+        if 'ICLUS_v3' in db:
+            continue
+        print(f"Vacuuming {db}...")
         con = sqlite3.connect(db)
         con.execute("VACUUM")
         con.close()
