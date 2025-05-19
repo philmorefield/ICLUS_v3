54415de67ca7bea82268112149559885484c74b2
philmorefield <philmorefield@users.noreply.github.com>
2025-05-19 06:56:45 -0400

    new census 2020 population

--- population/inputs/scripts/ACS/create_acs_gross_migration_weights.py
+++ population/inputs/scripts/ACS/create_acs_gross_migration_weights.py
@@ -14,11 +14,9 @@ MIGRATION_DB = os.path.join(DATABASE_FOLDER, 'migration.sqlite')
 POPULATION_DB = os.path.join(DATABASE_FOLDER, 'population.sqlite')
 ANALYSIS_DB = os.path.join(DATABASE_FOLDER, 'analysis.sqlite')
 ACS_DB = os.path.join(DATABASE_FOLDER, 'acs.sqlite')
-CENSUS_CSV_PATH = os.path.join(BASE_FOLDER, 'inputs\\raw_files\\Census\\2019')
+CENSUS_CSV_PATH = os.path.join(BASE_FOLDER, 'inputs\\raw_files\\Census\\2020\\decennial')
 
 ACS_FOLDER = os.path.join(BASE_FOLDER, 'inputs\\raw_files\\ACS\\2011_2015')
-
-years = ('2006-2010', '2011-2015')
 
 ACS_RACE_MAP = {1: 'WHITE', 2: 'BLACK', 3: 'ASIAN', 4: 'OTHER'}
 CENSUS_RACE_MAP = {'WA': 'WHITE',
@@ -155,6 +153,40 @@ def get_census_2010_2014_county_population_by_race_():
     return df
 
 
+def get_census_2020_county_population_by_race_():
+    csv_name = 'DECENNIALPL2020.P1-Data.csv'
+    csv = os.path.join(CENSUS_CSV_PATH, csv_name)
+    df = pd.read_csv(filepath_or_buffer=csv,
+                     skiprows=1,
+                     encoding='latin-1').iloc[:, :11]
+    df.columns = ['COFIPS', 'CYNAME', 'TOTAL_POPULATION', 'ONE_RACE', 'WHITE', 'BLACK', 'AIAN', 'ASIAN', 'NHPI', 'OTHER', 'TWO_OR_MORE']
+    df = df.drop(columns=['CYNNAME', 'TOTAL_POPULATION', 'OTHER'])
+    df['COFIPS'] = df['COFIPS'].str[-5:]
+
+    df['COFIPS'] = df['STATE'].astype(str).str.zfill(2) + df['COUNTY'].astype(str).str.zfill(3)
+    df = df.query('3 <= YEAR <= 7')
+    df = df.query('AGEGRP >= 1')
+    df = df.rename(columns={'AGEGRP': 'AGE_GROUP'})
+    df['AGE_GROUP'] = df['AGE_GROUP'].replace(to_replace=CENSUS_AGE_GROUP_MAP)
+
+    df['WHITE'] = df['WA_MALE'] + df['WA_FEMALE']
+    df['BLACK'] = df['BA_MALE'] + df['BA_FEMALE']
+    df['AIAN'] = df['IA_MALE'] + df['IA_FEMALE']
+    df['ASIAN'] = df['AA_MALE'] + df['AA_FEMALE']
+    df['NHPI'] = df['NA_MALE'] + df['NA_FEMALE']
+    df['TWO_OR_MORE'] = df['TOM_MALE'] + df['TOM_FEMALE']
+
+    df = df[['COFIPS', 'WHITE', 'BLACK', 'AIAN', 'ASIAN', 'NHPI', 'TWO_OR_MORE']]
+    df = df.melt(id_vars='COFIPS', var_name='RACE', value_name='POPULATION')
+    df = make_fips_changes(df)
+    df = df.groupby(by=['COFIPS', 'RACE'], as_index=False).mean()
+    df['POPULATION'] = (df['POPULATION'] / 5).astype(int)
+
+    df['SUM_OTHER'] = df.loc[df.RACE.isin(['AIAN', 'NHPI', 'TWO_OR_MORE'])].groupby(by=['COFIPS'])['POPULATION'].transform('sum')
+
+    return df
+
+
 def get_gross_migration_ratios_by_race():
     xl_filename = 'county-to-county-by-race-2011-2015-current-residence-sort.xlsx'
 
@@ -205,7 +237,7 @@ def get_gross_migration_ratios_by_race():
         df = pd.concat([df, temp], ignore_index=True)
     df = df.loc[df.RACE != 'OTHER']
 
-    origin_race = get_acs_2010_2014_county_population_by_race()
+    origin_race = get_census_2020_county_population_by_race_()
 
     df = df.merge(right=origin_race,
                   left_on=['ORIGIN_FIPS', 'RACE'],
@@ -216,35 +248,6 @@ def get_gross_migration_ratios_by_race():
     df.loc[df.RACE.isin(['AIAN', 'NHPI', 'TWO_OR_MORE']), 'TOTAL_FLOW'] = (df.ORIGIN_POPULATION / df.SUM_OTHER) * df.TOTAL_FLOW
 
     df = df.drop(columns=['ORIGIN_POPULATION', 'SUM_OTHER'])
-    df = df.groupby(['DESTINATION_FIPS', 'RACE'], as_index=False).sum()
-    df['RACE_SUM'] = df.groupby('RACE')['TOTAL_FLOW'].transform(sum)
-    df['WEIGHT_x_10^6'] = (df['TOTAL_FLOW'] / df['RACE_SUM']) * 1000000
-    df = df.pivot(index='DESTINATION_FIPS', columns='RACE', values='WEIGHT_x_10^6')
-    df.reset_index(inplace=True)
-    df.columns.name = None
-    df.fillna(value=0, inplace=True)
-    df = df[['DESTINATION_FIPS'] + list(RACE_MAP.values())]
-    dfs.append(df)
-
-    df1, df2 = dfs
-    new1 = df2[~df2.DESTINATION_FIPS.isin(df1.DESTINATION_FIPS)]
-    new1.loc[:, list(RACE_MAP.values())] = 0
-    df1 = df1.append(other=new1, ignore_index=True, verify_integrity=True, sort=True)
-    df1 = df1[['DESTINATION_FIPS'] + list(RACE_MAP.values())]
-    df1.sort_values(by='DESTINATION_FIPS', inplace=True)
-
-    new2 = df1[~df1.DESTINATION_FIPS.isin(df2.DESTINATION_FIPS)]
-    new2.loc[:, list(RACE_MAP.values())] = 0
-    df2 = df2.append(other=new2, ignore_index=True, verify_integrity=True, sort=True)
-    df2 = df2[['DESTINATION_FIPS'] + list(RACE_MAP.values())]
-    df2.sort_values(by='DESTINATION_FIPS', inplace=True)
-
-    df1.set_index(keys='DESTINATION_FIPS', inplace=True)
-    df2.set_index(keys='DESTINATION_FIPS', inplace=True)
-
-    df = (df1 + df2) / 2
-
-    df.reset_index(inplace=True)
 
     p = 'D:\\OneDrive\\ICLUS_v3\\population\\inputs\\databases'
     f = 'acs.sqlite'
--- population/inputs/scripts/create_tables_for_gravity_Census_1990_race.py
+++ population/inputs/scripts/create_tables_for_gravity_Census_1990_race.py
@@ -100,6 +100,7 @@ def get_census_immigration():
     df = df.drop(columns=['STATE', 'COUNTY'])
     df = make_fips_changes(df)
     df = df.set_index('COFIPS').mean(axis=1).round().astype(int).reset_index()
+    df.columns = ['COFIPS', 'IMMIGRATION']
 
     return df
 
